5c8ac545645c2e21080b19e78418d7e7ee0ba175
abadfish <kirsten.ofarrell@gmail.com>
2020-02-21 10:03:54 -0500

    added loader to login adn signup

--- src/views/Login/Signup.js
+++ src/views/Login/Signup.js
@@ -1,19 +1,40 @@
 import React, { useCallback } from 'react'
-import { useDispatch } from 'react-redux'
-import { Main, Heading } from 'grommet'
+import { useDispatch, useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import { Main, Heading, Box, Text } from 'grommet'
+import Loader from 'react-loader-spinner'
 import { signup } from '../../state/Auth/actions';
 import SignupForm from './SignupForm'
 
 const Signup = () => {
 
   const dispatch = useDispatch()
+  const makingRequestToApi = useSelector(state => state.appTransactions.makingRequestToAPI)
+
   const handleSignup = useCallback((user) => {
     dispatch(signup(user))
   })
   return (
     <Main pad='large'>
       <Heading className='login-heading' a11yTitle='BabyNamer' alignSelf='center' color='brand'>Anything But Bob</Heading>
-      <SignupForm onSubmit={ handleSignup } />
+      { makingRequestToApi ?
+        <Box alignSelf='center'>
+          <Loader
+            type='Puff'
+            color='#6FFFB0'
+            height={100}
+            width={100}
+          />
+        </Box>
+        :
+        <Box>
+          <Box alignSelf='center'>
+            <Text size='small' color='brand' margin='small'>Already have an account?
+            Login <NavLink style={{ color: '#6FFFB0' }} to='/login'>here.</NavLink></Text>
+          </Box>
+          <SignupForm onSubmit={ handleSignup } />
+        </Box>
+      }
     </Main>
   )
 
--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -8,7 +8,8 @@ import LoginForm from './LoginForm';
 
 const Login = () => {
   const dispatch = useDispatch()
-  const makingRequestToApi = useSelector(state => state.appTransactions.makingRequestToApi)
+  const makingRequestToApi = useSelector(state => state.appTransactions.makingRequestToAPI)
+  console.log(makingRequestToApi)
   const handleLogin = useCallback((user) => {
     dispatch(login(user))
   })
@@ -17,17 +18,23 @@ const Login = () => {
     <Main pad='large'>
       <Heading className='login-heading' a11yTitle='BabyNamer' alignSelf='center' color='brand'>Anything But Bob</Heading>
       { makingRequestToApi ?
-        <Loader
-          type='Puff'
-          color='#6FFFB0' 
-          height={100}
-          width={100}
-        /> : null }
-      <LoginForm onSubmit={ handleLogin } />
-      <Box alignSelf='center'>
-        <Text size='small' color='brand' margin='small'>New to Anything But Bob?
-        Create an account <NavLink style={{ color: '#6FFFB0' }} to='/signup'>here.</NavLink></Text>
-      </Box>
+        <Box alignSelf='center'>
+          <Loader
+            type='Puff'
+            color='#6FFFB0'
+            height={100}
+            width={100}
+          />
+        </Box>
+        :
+        <Box alignSelf='center'>
+          <LoginForm onSubmit={ handleLogin } />
+          <Box alignSelf='center'>
+            <Text size='small' color='brand' margin='small'>New to Anything But Bob?
+            Create an account <NavLink style={{ color: '#6FFFB0' }} to='/signup'>here.</NavLink></Text>
+          </Box>
+        </Box>
+      }
     </Main>
   )
 }
